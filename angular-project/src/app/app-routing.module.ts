d4c0db3304939e55cda7fdaa164750dc293d2226
TAVnotDove <slavei.dikovski123@gmail.com>
2022-11-28 22:57:51 +0200

    created a working basic navigation

--- angular-project/src/app/app-routing.module.ts
+++ angular-project/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "",
+  pathMatch: "full",
+  redirectTo: "/home",
+},
+{
+  path: "home",
+  component: HomeComponent
+},
+{
+  path: "register",
+  component: RegisterComponent
+}, {
+  path: "login",
+  component: LoginComponent
+},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-project/src/app/app.module.ts
+++ angular-project/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
