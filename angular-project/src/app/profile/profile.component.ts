0f868dbebc1c256f73c16ebb4711132bc6082e5c
TAVnotDove <slavei.dikovski123@gmail.com>
2022-12-13 10:58:30 +0200

    added real data to profile

--- angular-project/src/app/profile/profile.component.ts
+++ angular-project/src/app/profile/profile.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  form = this.fb.group({
+    email: [],
+    password: [],
+    validators: []
+  })
 
+  constructor(private fb: FormBuilder){}
+  
+  ngOnInit(): void {
+    const user = JSON.parse(localStorage.getItem("user")!);
+
+    this.form = this.fb.group({
+      email: [{
+        value: user.email,
+        disabled: true
+      }],
+      password: [{
+        value: user.password,
+        disabled: true
+      }],
+      validators: []
+    })
+  }
 }
