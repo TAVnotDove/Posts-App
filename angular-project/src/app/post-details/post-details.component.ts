242d5391502de06f07ca7eefbb9e56d2718e76e6
TAVnotDove <slavei.dikovski123@gmail.com>
2022-12-14 18:13:15 +0200

    made edit and delete buttons responsive

--- angular-project/src/app/post-details/post-details.component.ts
+++ angular-project/src/app/post-details/post-details.component.ts
@@ -9,15 +9,23 @@ import { PostsService } from '../services/posts.service';
 })
 export class PostDetailsComponent {
   post: any = null
+  isOwner: boolean = false
 
   constructor(private postsService: PostsService, private activatedRoute: ActivatedRoute){}
   
   ngOnInit(): void {
     const postId = this.activatedRoute.snapshot.paramMap.get("postId")
+    const user = JSON.parse(localStorage.getItem("user")!);
     
     this.postsService.getPost(postId!).subscribe({
       next: (v: any) => {
         this.post = v
+
+        if (user) {
+          if (v._ownerId === user._id) {
+            this.isOwner = true
+          }
+        }
       },
       error: (e) => {
         console.error(e)
