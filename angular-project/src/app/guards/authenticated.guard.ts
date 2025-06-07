45a3574322cd23330ffcd5cc98fb02e8c1252e44
TAVnotDove <slavei.dikovski123@gmail.com>
2025-06-07 23:43:40 +0300

    updated Angular to version 16

--- angular-project/src/app/guards/authenticated.guard.ts
+++ angular-project/src/app/guards/authenticated.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from "@angular/core"
-import { CanActivate, Router, UrlTree } from "@angular/router"
+import { Router, UrlTree } from "@angular/router"
 import { UserService } from "../services/user.service"
 
 @Injectable({
     providedIn: "root"
 })
-export class AuthenticatedGuard implements CanActivate {
+export class AuthenticatedGuard  {
     constructor(private router: Router, private userService: UserService) { }
 
     canActivate(): true | UrlTree {
--- angular-project/src/app/guards/not-authenticated.guard.ts
+++ angular-project/src/app/guards/not-authenticated.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from "@angular/core"
-import { CanActivate, Router, UrlTree } from "@angular/router"
+import { Router, UrlTree } from "@angular/router"
 import { UserService } from "../services/user.service"
 
 @Injectable({
     providedIn: "root"
 })
-export class NotAuthenticatedGuard implements CanActivate {
+export class NotAuthenticatedGuard  {
     constructor(private router: Router, private userService: UserService) { }
 
     canActivate(): true | UrlTree {
