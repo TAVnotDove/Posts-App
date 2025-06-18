c6080f93f46093f41d2e5b55cd9ee4eee1dc7064
TAVnotDove <slavei.dikovski123@gmail.com>
2025-06-18 23:49:49 +0300

    updated header and fixed theme change bg

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -1,22 +1,31 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from './services/user.service';
+import { ThemesService } from './services/themes.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   userIsLoggedIn: boolean = false;
 
-  constructor(private router: Router, private userService: UserService){}
+  constructor(
+    private router: Router,
+    private userService: UserService,
+    private themesService: ThemesService
+  ) {}
 
   ngOnInit() {
-    this.router.events.subscribe(event => {
-      if (event.constructor.name === "NavigationEnd") {
-        this.userIsLoggedIn = this.userService.authenticateUser()
+    this.router.events.subscribe((event) => {
+      if (event.constructor.name === 'NavigationEnd') {
+        this.userIsLoggedIn = this.userService.authenticateUser();
       }
-    })
+    });
+  }
+
+  getTheme(): string {
+    return this.themesService.getTheme();
   }
 }
