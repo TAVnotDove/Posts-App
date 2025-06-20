21ab682873ae17ef13b4327d8f128c97fd6b906c
TAVnotDove <slavei.dikovski123@gmail.com>
2025-06-20 23:51:08 +0300

    extended tailwind colors

--- angular-project/tailwind.config.js
+++ angular-project/tailwind.config.js
@@ -4,7 +4,26 @@ module.exports = {
     "./src/**/*.{html,ts}", // Add this line
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "pa-gray": {
+          100: "#eeeeee",
+          200: "#f5f5f5",
+          300: "#fafafa",
+          400: "#3a3a3a",
+          500: "#c2c3c4",
+        },
+        "pa-black": {
+          100: "#333333",
+          200: "#0f0f10",
+        },
+        "pa-red": {
+          100: "#dd0031",
+          200: "#c3002f",
+        },
+        "pa-blue": "#1976d2",
+      },
+    },
   },
   plugins: [],
-}
+};
